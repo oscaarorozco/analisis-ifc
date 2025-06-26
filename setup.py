d2b62b51d7f089abfb850af2cfd8e1533cd37679
Oscar Orozco <oscarorozcopedroviejo@gmail.com>
2025-06-26 10:59:12 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="ifc-app",
+    version="0.1",
+    install_requires=[
+        "ifcopenshell @ https://github.com/IfcOpenShell/IfcOpenShell/releases/download/v0.8.0/ifcopenshell-0.8.0-cp311-cp311-win_amd64.whl"
+    ],
+)
